d8c50cabc97de35efe454de2783c933eb9a93687
Chan <33647503+kryschanhi@users.noreply.github.com>
2020-04-26 18:22:57 +0800

    Added cinema and jquery

--- js/movies.js
+++ js/movies.js
@@ -11,7 +11,7 @@ var movies = [
 		rating: "8.4/10",
 		duration: "3h 2m",
 		date: '2020-04-28',
-		cinema: "",
+		cinema: ["1","2"],
 		synopsis: "Adrift in space with no food or water, Tony Stark sends a message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the remaining Avengers -- Thor, Black Widow, Captain America and Bruce Banner -- must figure out a way to bring back their vanquished allies for an epic showdown with Thanos -- the evil demigod who decimated the planet and the universe."
 	},
 	{
@@ -20,6 +20,7 @@ var movies = [
 		rating: "6.8/10",
 		duration: "2h 5m",
 		date: '2020-04-28',
+		cinema: ["3"],
 		synopsis: "To save her ailing father from serving in the Imperial Army, a fearless young woman disguises herself as a man to battle northern invaders in China."
 	},
 	{
@@ -28,6 +29,7 @@ var movies = [
 		rating: "6.6/10",
 		duration: "1h 40m",
 		date: '2020-04-28',
+		cinema: ["4"],
 		synopsis: "The world needed a hero -- it got a hedgehog. Powered with incredible speed, Sonic embraces his new home on Earth -- until he accidentally knocks out the power grid, sparking the attention of uncool evil genius Dr. Robotnik. Now, it's supervillain vs. supersonic in an all-out race across the globe to stop Robotnik from using Sonic's unique power to achieve world domination."
 	},
 	{
@@ -81,9 +83,22 @@ $(document).ready(function(){
 	var fDate = new Date("2020-04-28");
 	var lDate = new Date("2020-05-06");
 	var mDate;
+	var lTest;
+	var i;
+	var j;
+    var cinema = "";
 
-	for(var i = 0; i < movies.length; i++){
+	for(i = 0; i < movies.length; i++){
 		mDate = new Date(movies[i].date);
+
+
+        
+        if (movies[i].cinema) {
+        	cinema = 'Cinema: ';
+	      for (var j = 0; j < movies[i].cinema.length; j++) {
+	        cinema += movies[i].cinema[j] + ' ';
+	      }
+	    }
 		if(mDate <= lDate && mDate >= fDate){
 		 html += '<div class="col-lg-4 col-md-6 feature-item">' +
 		    '<a href="reservation.html">' +
@@ -98,6 +113,8 @@ $(document).ready(function(){
 		    'Duration: ' + movies[i].duration +
 		    '<br><br>' +
 		    'Synopsis: ' + movies[i].synopsis +
+		    '<br><br>' +
+		    cinema +
 		    '</div>' +
 		    '</div>' +
 		    '</div>' +
@@ -105,6 +122,7 @@ $(document).ready(function(){
 		    '</div>';
 
 		}
+        cinema = "";
 	}
 
 	$("#movies").html(html);
