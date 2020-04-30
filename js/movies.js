e751997cfb9e51ecff3cfaac590d77df0865a030
Chan <33647503+kryschanhi@users.noreply.github.com>
2020-04-30 13:53:01 +0800

    local storage
    local storage test -- somewhat ok for movies

--- js/movies.js
+++ js/movies.js
@@ -148,6 +148,39 @@ $(document).ready(function(){
 });
 
 
+
+if(localStorage) {
+  $(document).ready(function() {
+    localStorage.setItem("movies",JSON.stringify(movies));
+
+
+
+
+
+//to be used soon
+    // $(".save").click(function() {
+    //   // Get input name
+    //   var lastName = $("#lastName").val();
+      
+    //   // Store data
+    //     sessionStorage.setItem("last_name", lastName);
+    //   alert("Your last name is saved.");
+    // });
+    // $(".access").click(function() {
+    //   // Retrieve data
+    //     alert("Hi, " + localStorage.getItem("first_name") + " " + sessionStorage.getItem("last_name"));
+    // });
+  });
+} else {
+    alert("Sorry, your browser do not support local storage.");
+}
+
+
+
+
+
+
+
 //to be used soon
 // function dateCheck() {
 
--- js/test.js
+++ js/test.js
@@ -1,17 +1,27 @@
 if(localStorage) {
   $(document).ready(function() {
-    $(".save").click(function() {
-      // Get input name
-      var lastName = $("#lastName").val();
+
+    localStorage.setItem("movies",JSON.stringify())
+
+
+
+
+
+
+
+
+    // $(".save").click(function() {
+    //   // Get input name
+    //   var lastName = $("#lastName").val();
       
-      // Store data
-        sessionStorage.setItem("last_name", lastName);
-      alert("Your last name is saved.");
-    });
-    $(".access").click(function() {
-      // Retrieve data
-        alert("Hi, " + localStorage.getItem("first_name") + " " + sessionStorage.getItem("last_name"));
-    });
+    //   // Store data
+    //     sessionStorage.setItem("last_name", lastName);
+    //   alert("Your last name is saved.");
+    // });
+    // $(".access").click(function() {
+    //   // Retrieve data
+    //     alert("Hi, " + localStorage.getItem("first_name") + " " + sessionStorage.getItem("last_name"));
+    // });
   });
 } else {
     alert("Sorry, your browser do not support local storage.");
