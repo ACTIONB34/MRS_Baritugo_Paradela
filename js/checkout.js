1f8014e3150985e68fd16664be0130221278b638
Chan <33647503+kryschanhi@users.noreply.github.com>
2020-05-05 02:56:57 +0800

    Updates

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,11 @@
+/* =================================
+------------------------------------
+  MRS Web Checkout
+  Version: 1.0
+ ------------------------------------
+ ====================================*/
+
+
 /*------------------
   Checkout Details
 --------------------*/
@@ -54,4 +62,82 @@ $(document).ready(function(){
   $("#movieCinemaShowing").html(movieCinemaShowing);
   $("#movieSeats").html(movieSeats);
   $("#movieCost").html(movieCost);
+});
+
+
+/*------------------
+  Add Reservation
+--------------------*/
+
+  // var reservations = [];
+  // if(!localStorage.getItem("reservations")){
+  // localStorage.setItem("reservations",JSON.stringify(reservations));
+  
+  // }
+
+$(document).ready(function(){
+    var reservations = JSON.parse(localStorage.getItem("reservations"));
+    $.fn.addReservation = function(){ 
+      var seatsFinalCheckout = [];  
+      var i;
+      var j;
+      var movies = JSON.parse(localStorage.getItem("movies"));
+      var seatsCheckoutList = JSON.parse(sessionStorage.getItem("selectedSeats"));
+
+      var movieId = sessionStorage.getItem("movieId");
+
+      var movieName = movies[movieId-1].name;
+      var movieDate = sessionStorage.getItem("date");
+      var movieCinema = sessionStorage.getItem("cinemaId");
+      var movieShowing = sessionStorage.getItem("showing");
+      var movieCinemaShowing = movieCinema + " - " + movieShowing;
+      
+      var movieCost = sessionStorage.getItem("cost");
+
+
+      for(i = 0, j = 0; i < seatsCheckoutList.length; i++){
+        if(seatsCheckoutList[i] != null){
+          seatsFinalCheckout[j++] = seatsCheckoutList[i];
+        }
+      }
+
+
+      var reservation = {
+        id: ++(reservations.length),
+        movieId: movieId,
+        cinemaId: movieCinema,
+        seats: seatsFinalCheckout,
+        date: movieDate,
+        showing: movieShowing,
+        price: movieCost
+      };
+
+      var reservationsStorage = JSON.parse(localStorage.getItem("reservations"));
+      reservationsStorage.push(reservation);
+      localStorage.setItem("reservations",JSON.stringify(reservationsStorage));
+      sessionStorage.clear();
+      //location.reload();
+
+      
+      //also add reservedSeats
+
+      var success = '<div class="col-lg-12">' +
+                    '<div class="alert alert-success alert-dismissible fade show" role="alert">' +
+                    '<h4 class="alert-heading">Successfully Reserved!</h4>' +
+                    '<hr>' +
+                    '<p class="mb-0">Yay! Thank you for reserving at MovieRocket!</p>' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                    '<span aria-hidden="true">&times;</span>' +
+                    '</button>' +
+                    '</div>' +
+                    '</div>';
+
+      $('#exampleModalLong').modal('toggle');
+      $("#success").html(success);
+    }
+
+    $("#add-btn").click(function(){
+        $.fn.addReservation();
+    });
+
 });
